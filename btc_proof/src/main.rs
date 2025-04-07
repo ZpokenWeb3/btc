cd37bfa635e75e4493456ec45e499544fc13264e
EkaterynaKuznetsova <kate7smith12@gmail.com>
2025-04-07 14:07:43 +0000

    Make proofs for tx & PoW

--- btc_proof/src/main.rs
+++ btc_proof/src/main.rs
@@ -13,7 +13,7 @@ pub fn main() {
 
     for block_height in block_range {
         let block_filename = format!("../blocks/block_{}.json", block_height);
-        println!("Processing block: {}", block_filename);
+        println!("\nProcessing block: {}", block_filename);
         
         let start_time = Instant::now();
         
@@ -39,8 +39,6 @@ pub fn main() {
                 match result {
                     Ok((output, proof)) => {
                         let is_valid = verify_prev_hash(proof);
-                        let output_hash = hex::encode(reverse(&output));
-                        println!("Output prev_hash: {:#?}", output_hash);
                         println!("Valid proof: {}", is_valid);
                     }
                     Err(_) => {
@@ -76,21 +74,75 @@ pub fn main() {
             }
         }
 
+        // check tx & merkle root
+        // Convert all txids in bytes (LE -> BE)
+        let mut txids: Vec<Vec<u8>> = block.tx.iter()
+            .map(|tx| {
+                let mut bytes = hex::decode(&tx.hash).expect("Invalid txid hex");
+                bytes.reverse(); // txid are in little-endian
+                bytes
+            })
+            .collect();
+
+        let mut expected_root_bytes = hex::decode(&block.mrkl_root).expect("Invalid merkle_root hex");
+        expected_root_bytes.reverse(); // little-endian
+        println!("\nTx & merkle root check");
+        let start_time_tx = Instant::now();
+        let (prove_tx, verify_tx) = guest::build_check_tx();
+        // Check the first one, but it has to be provided separately
+        let result = std::panic::catch_unwind(|| prove_tx(txids[0].as_slice(), &expected_root_bytes, txids.clone()));
+        match result {
+            Ok((output, proof)) => {
+                let is_valid = verify_tx(proof);
+                println!("Block {}: Proof generated successfully", block_height);
+                println!("Valid proof: {}", is_valid);
+            }
+            Err(_) => {
+                println!("Block {}: Proof generation failed because tx does not match merke root!", block_height);
+            }
+        }
+        let elapsed_time_tx = start_time_tx.elapsed();
+        let elapsed_ms_tx = elapsed_time_tx.as_millis();
+        println!("Tx & merkle root processed in {} ms", elapsed_ms_tx);
+
+        // PoW check
+        let bits: [u8; 4] = header.bits();
+        println!("\nPoW check");
+        let start_time_pow = Instant::now();
+        let (prove_pow, verify_pow) = guest::build_check_proof_of_work();
+        let result = std::panic::catch_unwind(|| prove_pow(&expected_hash, &bits.to_vec()));
+        match result {
+            Ok((output, proof)) => {
+                let is_valid = verify_pow(proof);
+                println!("Block {}: Proof generated successfully", block_height);
+                println!("Valid proof: {}", is_valid);
+            }
+            Err(_) => {
+                println!("Block {}: Proof generation failed because PoW in not valid!", block_height);
+            }
+        }
+        let elapsed_time_pow = start_time_pow.elapsed();
+        let elapsed_ms_pow = elapsed_time_pow.as_millis();
+        println!("PoW processed in {} ms", elapsed_ms_pow);
+
+        // Block header check
         println!("\nHeader hash check");
+        let start_time_header = Instant::now();
         let (prove_hash, verify_hash) = guest::build_check_header_hash();
         let result = std::panic::catch_unwind(|| prove_hash(&expected_hash, &header.as_bytes()));
         match result {
             Ok((output, proof)) => {
                 let is_valid = verify_hash(proof);
-                let output_hash = hex::encode(reverse(&output));
                 println!("Block {}: Proof generated successfully", block_height);
-                println!("Output hash: {:#?}", output_hash);
                 println!("Valid proof: {}", is_valid);
             }
             Err(_) => {
                 println!("Block {}: Proof generation failed because hashes are different!", block_height);
             }
         }
+        let elapsed_time_header = start_time_header.elapsed();
+        let elapsed_ms_header = elapsed_time_header.as_millis();
+        println!("Header processed in {} ms", elapsed_ms_header);
 
         let elapsed_time = start_time.elapsed();
         let elapsed_ms = elapsed_time.as_millis();
